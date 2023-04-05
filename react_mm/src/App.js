2ecfe3f2a91be67ca9fba00b5c314d92cd529565
soo <sale4168000@gmail.com>
2023-04-05 12:18:46 +0000

    [add]first page

--- react_mm/src/App.js
+++ react_mm/src/App.js
@@ -1,84 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Component } from "react";
+import Home from "./pages/Home";
+import MusicRecom from "./pages/MusicRecom";
 
-import React, { useState } from 'react';
-import Plot from 'react-plotly.js';
 
-const RadarChart = () => {
-  const [data, setData] = useState([50,50,50,50,50,50]);
-
-  const onClick = (evt) => {
-    console.log(evt.points[0].y);
-  };
-
-  const onDataChanged = (index, value) => {
-    const newData = [...data];
-    newData[index] = value;
-    setData(newData);
-  };
-
-  const markerColors = ['#FF4136', '#FF851B', '#FFDC00', '#2ECC40', '#0074D9', '#B10DC9'];
-  const emotion = ['슬픔', '외로움', '분노', '사랑', '즐거움', '행복']
+function App() {
   return (
-    <div>
-      <Plot
-        data={[
-          {
-            type: 'scatterpolar',
-            r: data,
-            theta: emotion,
-            fill: 'toself',
-            hoverinfo: 'none',
-            mode: 'markers',
-            marker: {
-              size: 10,
-              color: markerColors,
-              opacity: 0.7
-            },
-            line: {
-              color: 'skyblue',
-              width: 2,
-              opacity: 0.7
-            }
-          }
-        ]}
-        layout={{
-          polar: {
-            gridshape: 'linear',
-            radialaxis: {
-              visible: true,
-              range: [0, 100]
-            }
-          },
-          margin: {
-            t: 50,
-            b: 50,
-            l: 50,
-            r: 50
-          },
-          width: 400,
-          height: 400,
-          hovermode: 'closest'
-        }}
-        onClick={onClick}
-      />
-      <div>
-        {data.map((value, index) => (
-          <div key={index}>
-            <span>{emotion[index]}: </span>
-            <input
-              type="number"
-              min={0}
-              max={100}
-              step={5}
-              value={value}
-              onChange={(evt) => onDataChanged(index, evt.target.value)} //parseFloat
-            />
-          </div>
-        ))}
+    <Router>
+      <div style={{ maxWidth: 1280, margin: "auto", padding: 10 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/MusicRecom" element={<MusicRecom />}></Route>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
-};
+}
 
-export default RadarChart;
-
+export default App;
---
+++ react_mm/src/pages/Home.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import Plot from 'react-plotly.js';
+
+const RadarChart = () => {
+  const [data, setData] = useState([50, 50, 50, 50, 50, 50]);
+
+  const onClick = (evt) => {
+    console.log(evt.points[0].y);
+  };
+
+  const onDataChanged = (index, value) => {
+    const newData = [...data];
+    newData[index] = value;
+    setData(newData);
+  };
+
+  const markerColors = ['#FF4136', '#FF851B', '#FFDC00', '#2ECC40', '#0074D9', '#B10DC9'];
+  const emotion = ['슬픔', '외로움', '분노', '사랑', '즐거움', '행복']
+  return (
+    <div>
+      <Plot
+        data={[
+          {
+            type: 'scatterpolar',
+            r: data,
+            theta: emotion,
+            fill: 'toself',
+            hoverinfo: 'none',
+            mode: 'markers',
+            marker: {
+              size: 10,
+              color: markerColors,
+              opacity: 0.7
+            },
+            line: {
+              color: 'skyblue',
+              width: 2,
+              opacity: 0.7
+            }
+          }
+        ]}
+        layout={{
+          polar: {
+            gridshape: 'linear',
+            radialaxis: {
+              visible: true,
+              range: [0, 100]
+            }
+          },
+          margin: {
+            t: 50,
+            b: 50,
+            l: 50,
+            r: 50
+          },
+          width: 400,
+          height: 400,
+          hovermode: 'closest'
+        }}
+        onClick={onClick}
+      />
+      <div>
+        {data.map((value, index) => (
+          <div key={index}>
+            <span>{emotion[index]}: </span>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              step={5}
+              value={value}
+              onChange={(evt) => onDataChanged(index, evt.target.value)} //parseFloat
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={e => {
+            musicRecommend(data).then(data => {
+              console.log(data);
+              navigate("/MusicRecom", { replace: true });
+            });
+          }}>
+          추천음악이동
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RadarChart;
+
