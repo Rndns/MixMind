1275184bdecc8cde827e4993c840b38b2af8e56e
seongjin <bible1218@naver.com>
2023-04-12 10:46:11 +0000

    [add]music player

--- react_mm/src/App.js
+++ react_mm/src/App.js
@@ -3,13 +3,14 @@ import React from "react";
 
 import MusicRecom from "./pages/MusicRecom";
 import MusicPlay from "./pages/MusicPlay";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <Router>
       <div style={{ maxWidth: 1280, margin: "auto", padding: 10 }}>
         <Routes>
-          <Route path="/" element={<MusicRecom />} />
+          <Route path="/" element={<Test />} />
           <Route path="/MusicRecom" element={<MusicRecom />}></Route>
           <Route path="/musicplaypage" element={<MusicPlay />}></Route>
         </Routes>
