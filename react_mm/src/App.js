80c0dbc0fddce134018e3b22bbcec45dcd40c5a2
hanjk <gkswnsrb8560@naver.com>
2023-05-01 07:08:24 +0000

    app.js-debug

--- react_mm/src/App.js
+++ react_mm/src/App.js
@@ -19,7 +19,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Info from "./pages/user/Info";
 import AutoComplete from "./pages/AutoComplete";
 import MusicTitle from './pages/MusicTitle';
-import MusicTitle from "./pages/MusicTitle";
 
 function App() {
   return (
