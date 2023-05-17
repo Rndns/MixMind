f6a68e53b314a40e9a0422ad7e0539d718ec11cb
hanjk <gkswnsrb8560@naver.com>
2023-05-17 06:58:10 +0000

    playlist-mk

--- react_mm/src/pages/playlist/PlayGroup.js
+++ react_mm/src/pages/playlist/PlayGroup.js
@@ -2,6 +2,7 @@ import "./PlayGroup.css";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Card, Button } from 'react-bootstrap';
+import del from '../../images/delete.png';
 
 export default function PlayGroup() {
     const location = useLocation()
@@ -16,23 +17,24 @@ export default function PlayGroup() {
     }, [])
 
     return (
-        <div className="playgroup">
-          <div className="playlist-header">
-            <h2><b>플레이리스트 제목{playGroup.name}</b></h2>
-            <Button variant="light" size="lg"><b>전체듣기</b></Button>
-            <Button variant="light" size="lg" onClick={editPlayGroup}><b>수정하기</b></Button>
-          </div>
+      <div className="playgroup">
+            <div className="my-playlist">
+              <h1><b>내 플레이리스트</b></h1>
+            </div>
             {playGroup && playGroup.map((playGroup) => (
-            <Card key={playGroup.id} style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={playGroup.name} />
-                <Card.Body>
-                    <Card.Title>{playGroup.name}</Card.Title>
-                    <Card.Text>
-                    {playGroup.name} - {playGroup.name}
-                    </Card.Text>
-                    <Button variant="danger">삭제</Button>
-                </Card.Body>
-            </Card>
+              <div className="playlist-header" key={playGroup.id}>
+                  <div className="playlist-cover">
+                    <img src="https://cdnimg.melon.co.kr/cm2/album/images/110/11/565/11011565_20220801102637_500.jpg?1d674a44faffa0ebd34d86c182463171/melon/resize/282/quality/80/optimize" alt='playlistImg'/>
+                  </div>
+                  <div className="header-str">
+                    <h1><b>{playGroup.name}</b></h1>
+                    <h4><b>2023 - 05 - 17</b></h4>
+                    <h4><b>#여행 #여름 #청량</b></h4>
+                  </div>
+                  <Button variant="light" size="lg"><b>상세보기</b></Button>
+                  <Button variant="danger" size="lg" onClick={editPlayGroup}><b>수정하기</b></Button>
+                  <img src={del} class="delete" alt={"삭제하기"} title={"플레이리스트 삭제하기"}/>
+              </div>
             ))}
         </div>
     )
